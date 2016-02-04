25116c31bf2b0e12b5aaac158de20cf3871f05ca
Felix Mann <djflix@gmail.com>
2016-02-04 20:03:17 +0100

    Add empty sketch and some resources

---
+++ code/oakpowerboard/oakpowerboard.ino
@@ -0,0 +1,14 @@
+#include "roboto_10.h"
+#include "roboto_12.h"
+#include "roboto_thin_20.h"
+#include "images.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

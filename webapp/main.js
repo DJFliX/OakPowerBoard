2260c6589c026253ea3d33b0248e51f9ead87cbe
Felix Mann <djflix@gmail.com>
2016-03-12 20:11:51 +0100

    Fix stupid typo/mistake in naming of the css element to add

--- webapp/main.js
+++ webapp/main.js
@@ -27,7 +27,7 @@ function addCss(id, url) {
     {
         var head  = document.getElementsByTagName('head')[0];
         var link  = document.createElement('link');
-        link.id   = cssId;
+        link.id   = id;
         link.rel  = 'stylesheet';
         link.type = 'text/css';
         link.href = url;

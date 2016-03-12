431ce3c8a32b51ba0f581adeacc85bcf49e7d347
Felix Mann <djflix@gmail.com>
2016-03-12 20:05:03 +0100

    Added Bootstrap and bootstrappy buttons :)

--- webapp/main.js
+++ webapp/main.js
@@ -1,3 +1,10 @@
+var buttons = [
+    { name: "Up", glyph: "glyphicon-menu-up"},
+    { name: "Right", glyph: "glyphicon-menu-right"},
+    { name: "Left", glyph: "glyphicon-menu-left"},
+    { name: "Down", glyph: "glyphicon-menu-down"}
+];
+
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -14,23 +21,39 @@ function button_callback(err, rsp) {
 	console.log(rsp);
 }
 
+function addCss(id, url) {
+    var head = document.getElementsByTagName("head")[0];
+    if (!document.getElementById(id))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = url;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+}
+
 function add(button_index) {
     //Create an input type dynamically.   
     var element = document.createElement("input");
     //Assign different attributes to the element. 
     element.type = 'button';
-    element.value = 'B' + button_index + ' (Up)'; // Really? You want the default value to be the type string?
+    element.class = 'btn btn-info';
+    var span = document.createElement("span");
+    span.class = "glyphicon " +  buttons[button_index].glyph;
+    element.appendChild(span);
     element.name = 'button' + button_index;  // And the name too?
     element.onclick = function() { // Note this is a function
         httpGetAsync("api/b" + button_index, button_callback)
     };
-
-    var foo = document.getElementById("bc");
-    //Append the element in page (in span).  
-    foo.appendChild(element);
+    document.getElementById("bc").appendChild(element);
 }
 
 function initialize() {
+    addCss('bootstrap', '//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css');
 	add(1);
 	add(2);
 	add(3);
